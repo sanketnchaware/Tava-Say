f95a7798f22ee717564e1d2c03c0a32420a837b1
Sanket Chaware <sanketnchaware@gmail.com>
2023-07-15 22:48:29 +0530

    added locate us section

--- src/Components/Homepage/HeroSection.tsx
+++ src/Components/Homepage/HeroSection.tsx
@@ -5,7 +5,7 @@ const HeroSection = () => {
         <div className="flex gap-10 justify-center  items-center mx-44">
             <div className='py-9'>
                 <h3 className="text-FloralWhite">Welcome to 
-                <br />
+
                 Tava Say ! </h3>
                 <br />
                 <p  className="text-xl text-FloralWhite">" Step into a realm of North Indian culinary bliss, where centuries-old traditions blend harmoniously with modern flavors, tantalizing your taste buds with every savory morsel. "</p>
